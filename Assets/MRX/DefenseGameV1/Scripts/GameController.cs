d971f83da98f3a4e6b3d7e3a9563941cb4ae1815
Xuyen <tranvanxuyen111@gmail.com>
2025-05-28 14:46:27 +0700

    Modify hero, enemy. Create GameController script

---
+++ Assets/MRX/DefenseGameV1/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+namespace MRX.DefenseGameV1
+{
+    public class GameController : MonoBehaviour
+    {
+        public float spawnTime;
+        public Enemy[] enemyPrefabs;
+        private bool m_isGameover;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            StartCoroutine(SpawnEnemy());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        IEnumerator SpawnEnemy()
+        {
+            while (!m_isGameover)
+            {
+                if (enemyPrefabs != null && enemyPrefabs.Length > 0)
+                {
+                    int randIdx = Random.Range(0, enemyPrefabs.Length);//(0,3) 0 1 2
+                    Enemy loclaEnemyPrefabs = enemyPrefabs[randIdx];
+                    if (loclaEnemyPrefabs)
+                    {
+                        Instantiate(loclaEnemyPrefabs, new Vector3(8, 0, 0), Quaternion.identity);
+                    }
+                }
+                yield return new WaitForSeconds(spawnTime);
+            }
+
+        }
+    }
+
+}
